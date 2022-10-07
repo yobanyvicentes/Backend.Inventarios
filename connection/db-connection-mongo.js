d3a4ac49ede3675861fcb2eb5b8477b67b755d67
yobanyvicentes <yobany.vicentes@est.iudigital.edu.co>
2022-10-06 22:52:17 -0500

    actualizacion

--- connection/db-connection-mongo.js
+++ connection/db-connection-mongo.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 //importar dotenv para las variables de entorno
 require('dotenv').config();
 //crear funcion getConnection asincrona:
-const usuarioAtlas = process.env.user;
-const passAtlas = process.env.pass;
+const usuarioAtlas = process.env.USER;
+const passAtlas = process.env.PASS;
 const getConnection = async () => {
     try {
         const url = `mongodb://${usuarioAtlas}:${passAtlas}@ac-cgnksfd-shard-00-00.qufnfy5.mongodb.net:27017,ac-cgnksfd-shard-00-01.qufnfy5.mongodb.net:27017,ac-cgnksfd-shard-00-02.qufnfy5.mongodb.net:27017/inventarios-app?ssl=true&replicaSet=atlas-3cfvg9-shard-0&authSource=admin&retryWrites=true&w=majority`;
